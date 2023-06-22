bb6ab01d1580bbe4deee6048fdf78bda349393d3
AdamJednacz <adamjednacz4@gmail.com>
2023-06-23 00:45:38 +0200

    kalkulator cały

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
-
 //zmienne do produktu//
-const product = document.getElementById("products")
+const numbers = document.querySelectorAll(".form__input")
+const checkboxs = document.querySelectorAll("input[type = checkbox]")
 const listProduct = document.querySelector(".list__item[data-id = products]")
 const productCalc = document.querySelector(".list__item[data-id = products] .item__calc")
 const productPrice = document.querySelector(".list__item[data-id = products] .item__price")
 //zmienne dla order
-const order = document.getElementById("orders")
 const listOrder = document.querySelector(".list__item[data-id = orders]")
 const orderCalc = document.querySelector(".list__item[data-id = orders] .item__calc")
 const orderPrice = document.querySelector(".list__item[data-id = orders]  .item__price")
 //zmienne dla Accounting
-const accounting = document.getElementById("accounting")
 const listAccounting = document.querySelector(".list__item[data-id = accounting]")
 const accountingPrice = document.querySelector(".list__item[data-id = accounting]  .item__price")
 //zmiene dla Terminal
-const terminal = document.getElementById("terminal")
 const listTerminal = document.querySelector(".list__item[data-id = terminal]")
 const terminalPrice = document.querySelector(".list__item[data-id = terminal]  .item__price")
 // zmienne dla choose package
@@ -27,81 +24,94 @@ const optionsCalc = document.querySelector(".list__item[data-id = package] .item
 const optionsPrice = document.querySelector(".list__item[data-id = package] .item__price")
 // zmienne dla total
 const total = document.getElementById("total-price")
-const listItems = document.querySelectorAll("input");
 const totalPrice = document.querySelector(".total__price")
 //
 
-//kod dla ceny prodktów//
-product.addEventListener("input", handleAddValue)
-function handleAddValue(){
-        listProduct.classList.add("open");
-        productCalc.innerText = product.value + " * " + "$0.5"
-
-        productPrice.innerText = "$"+ product.value * 0.5
 
 
-}
-//kod dla ceny zamówień //
-order.addEventListener("input", handleAddValueOrder)
-function handleAddValueOrder(){
-    listOrder.classList.add("open");
-    orderCalc.innerText = order.value + " * " + "$0.25"
-    orderPrice.innerText = "$"+ order.value * 0.25
+//kod dla products i orders
+numbers.forEach(function (number){
+    number.addEventListener("input" , handleAddValue)
+    productPrice.innerText = 0
+    orderPrice.innerText = 0
+    function handleAddValue(){
+        if(number.id === "products"){
+            listProduct.classList.add("open");
+            productCalc.innerText = number.value + " * " + "$0.5"
+            productPrice.innerText = "$" + number.value * 0.5
+        }if (number.id ==="orders"){
+            listOrder.classList.add("open");
+            orderCalc.innerText = number.value + " * " + "$0.25"
+            orderPrice.innerText = "$" + number.value * 0.25
+        }
+        calculateTotal()
+    }
+})
+//kod dla checkboxów
+checkboxs.forEach(function (checkbox){
+    checkbox.addEventListener("input" , handleAddCheckbox)
+    accountingPrice.innerText = 0
+    terminalPrice.innerText = 0
+    function handleAddCheckbox() {
 
-}
-
-//kod dla accounting//
-accounting.addEventListener("input" , handleAddAccounting)
-function handleAddAccounting(){
-    listAccounting.classList.toggle("open")
-    accountingPrice.innerText = "$35"
-}
-//kod dla terminal
-terminal.addEventListener("input" , handleAddTerminal)
-function handleAddTerminal(){
-    listTerminal.classList.toggle("open")
-
-    terminalPrice.innerText = "$5"
-}
-//kod do rozwinięcia menu i zwinięcia
-
-choosePackage.addEventListener("click" , handleShowOptions)
-function handleShowOptions(){
+        if (checkbox.id === "accounting") {
+            accountingPrice.innerText = "$" + 35
+            listAccounting.classList.add("open")
+            calculateTotal()
+        }
+        if (checkbox.id === "terminal") {
+            terminalPrice.innerText = "$" + 5
+            listTerminal.classList.add("open")
+            calculateTotal()
+        }
+        if (checkbox.checked === false && checkbox.id === "accounting") {
+            accountingPrice.innerText = 0
+            listAccounting.classList.remove("open")
+            calculateTotal()
+        }
+        if (checkbox.checked === false && checkbox.id === "terminal") {
+            terminalPrice.innerText = 0
+            listTerminal.classList.remove("open")
+            calculateTotal()
+        }
+    }
+})
+// //kod do wyboru opcji
+choosePackage.addEventListener("click", handleShowOptions)
+function handleShowOptions() {
     list1.classList.toggle("open")
 }
+optionsValue.forEach(function (optionValue) {
+    optionValue.addEventListener("click", handleAddOption)
+    optionsPrice.innerText = 0
+    function handleAddOption() {
 
-//kod do wyboru opcji
-
-optionsValue.forEach(function (optionValue){
-    optionValue.addEventListener("click" , handleAddOption)
-    function handleAddOption (){
         selectInput.innerText = optionValue.dataset.value
         listOptions.classList.add("open")
         optionsCalc.innerText = optionValue.dataset.value
 
-        if (optionValue.dataset.value === "Basic"){
-            optionsPrice.innerText = "$0"
+        if (optionValue.dataset.value === "Basic") {
+            optionsPrice.innerText = "$" + 0
         }
-        if (optionValue.dataset.value === "Professional"){
-
-            optionsPrice.innerText = "$25"
+        if (optionValue.dataset.value === "Professional") {
+            optionsPrice.innerText = "$" + 25
         }
-        if (optionValue.dataset.value === "Premium"){
-            optionsPrice.innerText = "$60"
+        if (optionValue.dataset.value === "Premium") {
+            optionsPrice.innerText = "$" + 60
         }
-        return optionsPrice.innerText
+        calculateTotal()
     }
-
 })
-// kod do total
-
-listItems.forEach(function (listItem){
-    listItem.addEventListener("input" , handleAddTotal)
-    function handleAddTotal(){
-        total.classList.add("open")
-    }
-    totalPrice.innerText =1
-})
+function calculateTotal() {
+    const productTotal = Number(productPrice.innerText.replace('$', ''));
+    const orderTotal = Number(orderPrice.innerText.replace('$', ''));
+    const accountingTotal = Number(accountingPrice.innerText.replace('$', ''));
+    const terminalTotal = Number(terminalPrice.innerText.replace('$', ''));
+    const optionsTotal = Number(optionsPrice.innerText.replace('$', ''));
 
 
+    const totalSum = productTotal + orderTotal + accountingTotal + terminalTotal + optionsTotal;
 
+    total.classList.add("open")
+    totalPrice.innerText = '$' + totalSum.toFixed(2);
+}
